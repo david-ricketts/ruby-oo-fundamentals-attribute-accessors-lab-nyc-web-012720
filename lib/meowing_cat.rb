4b953a6e2b2a688b4479777bd7ec03cf2018e735
David Ricketts <david.ricketts119@gmail.com>
2020-01-29 19:47:47 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,4 +1,17 @@
 ## code your solution here. 
 
 
-
+class Cat 
+  
+  def initializer (name) 
+    @name = name 
+  end
+  
+  def name
+    @name
+  end
+  
+  def meow
+    puts "meow!"
+  end
+end
